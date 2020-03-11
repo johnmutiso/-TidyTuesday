ebc5a0f06e7073e9fe595d6f047f0ec5f6383530
johnmutiso <johnmutish@gmail.com>
2020-03-11 22:15:01 +0300

    updating the script and plots

--- 2020/week 11/script.R
+++ 2020/week 11/script.R
@@ -29,55 +29,56 @@ cost_potential_pay_1 <- cost_potential_pay %>%
          out_state_total_cat = factor(case_when(out_of_state_total > 50000 ~ 1,
                                                 between(out_of_state_total, 30000,50000)~2,
                                                 out_of_state_total < 30000 ~ 3),
-                                      label = c('Above \n$50,000','Between \n$30,000 \n& $50,000','Below \n$30,000')),
+                                      label = c('Above \n$50,000','Between \n$30,000 \n& $50,000',
+                                                'Below \n$30,000')),
          in_state_total_cat = factor(case_when(in_state_total > 50000 ~ 1,
                                                between(in_state_total, 30000,50000)~2,
                                                in_state_total < 30000 ~ 3),
-                                     label = c('Above \n$50,000','Between \n$30,000 \n& $50,000','Below \n$30,000')),
+                                     label = c('Above \n$50,000','Between \n$30,000 \n& $50,000',
+                                               'Below \n$30,000')),
          state = factor(state),
          type = factor(type)) %>% 
-  select(state, type, degree_length, early_career_cat, mid_career_cat, in_state_total_cat, out_state_total_cat)
+  select(state, type, degree_length, early_career_cat, mid_career_cat, in_state_total_cat,
+         out_state_total_cat)
 
 
 cols <- c('#fe3434','#1fbb0a')
-tuition_state_cat <- 'in'
 
-# Plot <<<Tuition - potential pay>>>
+# Plot <<<Tuition fee - potential pay>>>
+# Plot 1 for in state <<in_state_total_cat>> and plot2 for <<out_state_total_cat>> as the final grouping var and axis1
 plot <- cost_potential_pay_1 %>% 
-  group_by(state, type, degree_length, early_career_cat, mid_career_cat, in_state_total_cat) %>% 
+  group_by(state, type, degree_length, early_career_cat, mid_career_cat, out_state_total_cat) %>% 
   summarise(freq = n()) %>% 
   arrange(type, freq) %>% 
-  ggplot(aes(axis1 = in_state_total_cat,
-             axis2 = early_career_cat, 
-             axis3 = mid_career_cat,
-             y = freq)) +
-  geom_alluvium(aes(fill = type),
-                width = 1/2) +
-  scale_fill_manual(values = cols) +
-  scale_y_continuous(breaks = seq(0,600, 100), expand = expansion(add = c(0.5,0.5))) +
-  geom_stratum(width = 1/4, alpha = 0.8, fill = '#040301', col = '#5f5f5d', lwd = 1.4) +
-  geom_text(stat = 'stratum', infer.label = TRUE, size = 4, 
-            fontface = 'bold', col = '#d2cfc2') +
-  scale_x_discrete(limits = c(paste0('Tuition Fee \nfor [',tuition_state_cat,'_State]\nResidents ($)'),
-                              'Potential \nEarly-Career \nSalary ($)',
-                              'Potential \nMid-Career \nSalary ($)'),
-                   expand = c(0.01, 0.01)) +
-  labs(y = "Number of Institutions", 
-       title = paste0("SALARY($) POTENTIAL BASED ON TUITION FEES [",toupper(tuition_state_cat), "-STATE]"), 
-       subtitle = paste0("[",toupper(tuition_state_cat), "-STATE]", "Residents"), 
-       caption = "Github: @johnmutiso\ndata: TuitionTracker.org\n2020-week 11 TidyTuesday") + 
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = '#0f0809'), 
-        panel.grid = element_blank(), 
-        axis.text = element_text(color = '#d2cfc2', size = 12, face = 'bold'), 
-        legend.text = element_text(size = 12, color = '#d2cfc2'),
-        #legend.position = 'bottom', 
-        legend.title = element_blank(),
-        axis.title.y = element_text(color = '#d2cfc2', size = 12),
-        axis.title = element_blank(),
-        plot.caption = element_text(colour = 'grey50', face = 'italic', size = 8),
-        plot.title  = element_text(colour = 'grey90', face = 'bold', size = 13),
-        plot.subtitle = element_text(colour = 'grey80', face = 'bold.italic', size = 10))
+  ggplot(aes(axis1 = out_state_total_cat, axis2 = early_career_cat, axis3 = mid_career_cat, y = freq)) +
+    geom_alluvium(aes(fill = type), width = 1/2) +
+    scale_fill_manual(values = cols) +
+    scale_y_continuous(breaks = seq(0,600, 100), expand = expansion(add = c(0.5,0.5))) +
+    geom_stratum(width = 1/4, alpha = 0.8, fill = '#040301', col = '#5f5f5d', lwd = 1.4) +
+    geom_text(stat = 'stratum', infer.label = TRUE, size = 4, 
+              fontface = 'bold', col = '#d2cfc2') +
+    scale_x_discrete(limits = c('Tuition Fee ($) for\n [OUT-OF-STATE]\nResidents',
+                                'Potential \nEarly-Career \nSalary ($)',
+                                'Potential \nMid-Career \nSalary ($)'),
+                     expand = c(0.01, 0.01)) +
+    labs(y = "Number of Institutions", 
+         title = "EARLY & MID-CAREER SALARY($) POTENTIAL BASED ON TUITION FEES", 
+         subtitle = "[OUT-OF-STATE] Residents", 
+         caption = "Github: @johnmutiso\ndata: TuitionTracker.org\n2020-week 11 TidyTuesday") + 
+    theme_minimal() +
+    theme(plot.background = element_rect(fill = '#0f0809'), 
+          panel.grid = element_blank(), 
+          axis.text = element_text(color = '#d2cfc2', size = 12, face = 'bold'), 
+          legend.text = element_text(size = 12, color = '#d2cfc2'),
+          legend.position = 'top', 
+          legend.title = element_blank(),
+          axis.title.y = element_text(color = '#d2cfc2', size = 12),
+          axis.title = element_blank(),
+          plot.caption = element_text(colour = 'grey50', face = 'italic', size = 8),
+          plot.title  = element_text(colour = 'grey90', face = 'bold', size = 13),
+          plot.subtitle = element_text(colour = 'grey80', face = 'bold.italic', size = 10))
 
-ggsave(filename = 'week 11 plot.jpeg', plot = plot, device = 'jpeg', path = '2020/week 11/',dpi = 500,
-       width = 11, height = 5.8)
+ggsave(filename = 'week 11 plot2.jpeg', plot = plot, device = 'jpeg', path = '2020/week 11/',dpi = 400,
+       width = 9, height = 7.2)
+
+
