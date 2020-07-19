7c35328f64c4fd640bcfc59e34d8e0a305e6732f
John Mutiso <johnmutish@gmail.com>
2020-07-19 16:44:40 +0300

    update week 28 and 29

--- 2020/week 28/script.R
+++ 2020/week 28/script.R
@@ -1,7 +1,7 @@
 #' -------------------------------------------------------------------------------
-#' INPUT THE SUBJECT TITLE OF THE DATASET
-#' The data this week comes from [SOURCE_OF_DATA](URL_TO_DATA). 
-#' This [ARTICLE_SOURCE](LINK_TO_ARTICLE) talks about SUBJECT TITLE in greater detail.
+#' Coffee Ratings
+#' The data this week comes from [Coffee Ratings](https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-07-07/readme.md). 
+#' This [Yorgos Askalidis - TWD](https://towardsdatascience.com/the-data-speak-ethiopia-has-the-best-coffee-91f88ed37e84) talks about Coffee Rating in greater detail.
 #' Credit: [John Mutiso](twitter: @johnmutiso_)
 #' --------------------------------------------------------------------------------
 
@@ -15,15 +15,38 @@ source('read_TT_data.R')
 read_TTdata(week = week_num)
 
 # plot
-plot <- ggplot()
+plot <- 
+    coffee_ratings %>%
+    filter(altitude_mean_meters < 9000) %>% 
+    group_by(country_of_origin, species) %>% 
+    summarise(mean_points = mean(total_cup_points),
+              altitude = mean(altitude_mean_meters),
+              flavor = mean(flavor),
+              sweetness = mean(sweetness),
+              aftertaste = mean(aftertaste)) %>% 
+    ggplot(aes(sweetness, reorder(country_of_origin, desc(sweetness)), size = flavor, fill = aftertaste, color = species)) +
+    geom_point(shape = 21) +
+    scale_fill_viridis_c('After\n    taste') +
+    scale_color_manual(values = c('#ff0000','snow1')) +
+    scale_size_binned(nice.breaks = T) +
+    labs(y = "", title = 'Coffee Ratings', 
+         caption = "Credit: @johnmutiso\nData: Coffee Quality Database via James LeDoux (@jmzledoux)\nGraphic: TidyTuesday Week 28") +
+    theme_minimal()+
+    theme(plot.background = element_rect(fill = '#20b2aa'),
+          panel.grid = element_line(colour = '#468499'), 
+          axis.text = element_text(size = 12, face = 'bold'),
+          plot.title = element_text(size = 20, family = 'Tahoma', hjust = 0.5))
+
 
 # save plot
 ggsave(
     plot = plot,
     height = 8.8,
-    width = 3.5,
+    width = 8,
     dpi = 500,
     device = 'png',
     filename = paste0('week',week_num,'plot.png'),
     path = paste0('2020/week ', week_num)
 )
+
+# Data: James LeDoux (@jmzledoux)
--- 2020/week 29/script.R
+++ 2020/week 29/script.R
@@ -1,10 +1,14 @@
 #' -------------------------------------------------------------------------------
-#' INPUT THE SUBJECT TITLE OF THE DATASET
-#' The data this week comes from [SOURCE_OF_DATA](URL_TO_DATA). 
-#' This [ARTICLE_SOURCE](LINK_TO_ARTICLE) talks about SUBJECT TITLE in greater detail.
+#' Astronaut database
+#' The data this week comes from [Mariya Stavnichuk and Tatsuya Corlett.](https://data.mendeley.com/datasets/86tsnnbv2w/1).
+#' This [Population analysis of space travelers](https://doi.org/10.1016/j.lssr.2020.06.003) talks about Astronauts in greater detail.
 #' Credit: [John Mutiso](twitter: @johnmutiso_)
 #' --------------------------------------------------------------------------------
 
+# download data from
+# tuesdata <- tidytuesdayR::tt_load(2020, week = 29)
+
+rm(list = ls())
 week_num = 29
 #libraries -----------------------------------------------------------------------------
 library(tidyverse)
@@ -14,16 +18,78 @@ loadfonts(device = 'win')
 source('read_TT_data.R')
 read_TTdata(week = week_num)
 
+
 # plot
-plot <- ggplot()
+plot <-
+    astronauts %>%
+    mutate(
+        age_at_selection = year_of_selection - year_of_birth,
+        yrs_2_misson_frm_selection = year_of_mission - year_of_selection,
+        age_at_mission = year_of_mission - year_of_birth,
+        point_s = runif(1277, 40, 77),
+        point_s = ifelse(str_detect(nationality, "U\\.S\\.|Russia"), NA, point_s)
+        
+    ) %>%
+    ggplot() +
+    geom_point(
+        aes(reorder(nationality, desc(age_at_mission), FUN = median), point_s),
+        position = position_jitter(width = 1),
+        color = 'grey80',
+        shape = 4,
+        size = 1 / 10
+    ) +
+    geom_point(
+        aes(
+            y = age_at_mission,
+            x = reorder(nationality, desc(age_at_mission), FUN = median),
+            fill = military_civilian,
+        ),
+        shape = 21,
+        size = 4,
+        col = 'snow1',
+        alpha = 0.7,
+        position = position_jitter(width = 0.3)
+    ) +
+    geom_text(
+        aes(x = 17, y = 75,
+            label = 'JOHN GLENN (Year: 1961)\n Mission: FRIENDSHIP 7 @77yrs,\n    The first American to orbit\n    the Earth, 5th person and 3rd\n    American in space'),
+        hjust = 1,
+        size = 2.5,
+        family = 'Segoe Print',
+        col = '#ffd700'
+    ) +
+    labs(
+        y = 'Age (yrs) going to mission',
+        x = '',
+        title = 'SPACE MISSIONS',
+        caption = "GitHub: @johnmutiso\nData: https://doi.org/10.1016/j.lssr.2020.06.003 via (@geokaramanis)\nGraphic: TidyTuesday week 29"
+    ) +
+    coord_polar(clip = 'on', start = 80) +
+    theme_minimal(base_rect_size = 0) +
+    theme(
+        axis.text.y = element_text(size = 6),
+        plot.background = element_rect(fill = '#666666'),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major = element_line(color = '#6897bb'),
+        axis.text.x = element_text(color = '#e9f8ff', size = 12, face = 'bold'),
+        axis.text.y.left = element_text(size = 15, color = '#d2b48c'),
+        legend.title = element_blank(),
+        legend.position = c(0.9, 0.1),
+        legend.text = element_text(size = 15, color = '#5ac18e'),
+        axis.title.y = element_text(size = 14, color = '#5ac18e'),
+        plot.caption = element_text(color = '#dcedc1', size = 7),
+        plot.title = element_text(size = 30, hjust = 0.5, family = 'Stencil', color = '#5ac18e')
+    )
 
 # save plot
 ggsave(
     plot = plot,
-    height = 8.8,
-    width = 3.5,
+    height = 10.2,
+    width = 9.7,
     dpi = 500,
     device = 'png',
-    filename = paste0('week',week_num,'plot.png'),
+    filename = paste0('week', week_num, 'plot.png'),
     path = paste0('2020/week ', week_num)
 )
+
+#data: https://doi.org/10.1016/j.lssr.2020.06.003 via Georgios Karamanis (@geokaramanis)
